c04e0fda38b30865de8f9f3686ab4f01fcb8f34a
bzone <yarnbCoder@gmail.com>
2018-05-07 11:14:54 +0800

    修改todo项目依赖

--- todoMVC/src/app.js
+++ todoMVC/src/app.js
@@ -1,6 +1,11 @@
 App({
-  onLaunch: function() {
-    console.log('App Launch');
+  async onLaunch() {
+    const n = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(1);
+      }, 3000);
+    });
+    console.log(n);
   },
   onShow: function() {
     console.log('App Show');
