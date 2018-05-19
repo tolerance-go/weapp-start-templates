6346b900b0b510cc11cc3c5bd45e7c2aaa1be8ee
bzone <yarnbCoder@gmail.com>
2018-05-19 20:20:42 +0800

    install postcss

--- todoMVC/weapp.config.js
+++ todoMVC/weapp.config.js
@@ -8,7 +8,26 @@ module.exports = {
   ],
   env: {
     production: {
-      plugins: ['weapp-plugin-jsmin', 'weapp-plugin-filemin', 'weapp-plugin-imgmin'],
+      plugins: [
+        [
+          'weapp-plugin-jsmin',
+          {
+            extra: true,
+          },
+        ],
+        [
+          'weapp-plugin-postcss',
+          {
+            plugins: [
+              require('autoprefixer')({
+                browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6'],
+              }),
+            ],
+          },
+        ],
+        'weapp-plugin-filemin',
+        'weapp-plugin-imgmin',
+      ],
     },
   },
 };
